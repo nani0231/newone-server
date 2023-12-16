ee382e67caf46bdc9891c6efe3ffd84ace5aac8c
Saiprakash2115 <saiprakash.98483@gmail.com>
2023-12-16 12:40:34 +0530

    update

--- server.js
+++ server.js
@@ -20,7 +20,6 @@ const port = 4010;
 
 const mogoURL =
   "mongodb+srv://badasiva22:Siva991276@cluster0.iis7lrd.mongodb.net/perfex-stack-project?retryWrites=true&w=majority";
-  // "mongodb+srv://saiprakash2115:m1Yb7ZlsB0nVVGbY@cluster0.r19eo2o.mongodb.net/skillhub2?retryWrites=true&w=majority"
   
 app.use(express.json());
 app.use(cors({ origin: "*" }));
